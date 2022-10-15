97b576c15d663cee696e392f75e495c644ff9813
Alex <lepsalex@gmail.com>
2022-10-15 15:01:43 -0400

    bullet collisions (super basic) and entity death

--- src/main.rs
+++ src/main.rs
@@ -33,6 +33,8 @@ fn main() {
         .add_system(entity_despawn)
         .add_system(move_targets)
         .add_system(move_bullets)
+        .add_system(bullet_collision)
+        .add_system(death)
         .run();
 }
 
@@ -169,6 +171,28 @@ fn move_targets(mut targets: Query<(&Target, &mut Transform)>, time: Res<Time>) 
 fn move_bullets(mut bullets: Query<(&Bullet, &mut Transform)>, time: Res<Time>) {
     for (bullet, mut transform) in &mut bullets {
         transform.translation += bullet.direction.normalize() * bullet.speed * time.delta_seconds();
+    }
+}
+
+fn bullet_collision(mut commands: Commands, bullets: Query<(Entity, &GlobalTransform), With<Bullet>>, mut targets: Query<(&mut Health, &Transform), With<Target>>) {
+    // simplistic collision system, consider physics engine
+    for (bullet, bullet_transform) in &bullets {
+        for (mut health, target_transform) in &mut targets {
+            // TODO: magic number for collision distance
+            if Vec3::distance(bullet_transform.translation(), target_transform.translation) < 0.2 {
+                commands.entity(bullet).despawn_recursive();
+                health.value -= 1;
+                break;
+            }
+        }
+    }
+}
+
+fn death(mut commands: Commands, targets: Query<(Entity, &Health)>) {
+    for (ent, health) in &targets {
+        if health.value <= 0 {
+            commands.entity(ent).despawn_recursive();
+        }
     }
 }
 
