6179baeda433242a5324ea45bfa3f7b575a34adc
Alex Lepsa <lepsalex@gmail.com>
2022-10-14 20:37:30 -0400

    basic plane and cube

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,45 @@
+use bevy::prelude::*;
+
+pub const WIDTH: f32 = 1280.0;
+pub const HEIGHT: f32 = 720.0;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .insert_resource(ClearColor(Color::rgb(0.2, 0.2, 0.2)))
+        .insert_resource(WindowDescriptor {
+            width: WIDTH,
+            height: HEIGHT,
+            title: "Learn Bevy 1.0".to_string(),
+            resizable: false,
+            ..default()
+        })
+        .add_startup_system(spawn_camera)
+        .add_startup_system(spawn_basic_command)
+        .add_plugins(DefaultPlugins)
+        .run();
+}
+
+fn spawn_camera(mut commands: Commands) {
+    commands.spawn_bundle(Camera3dBundle {
+        transform: Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
+        ..default()
+    });
+}
+
+fn spawn_basic_command(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    commands.spawn_bundle(PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Plane { size: 5.0 })),
+        material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
+        ..default()
+    });
+    commands.spawn_bundle(PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
+        material: materials.add(Color::rgb(0.67, 0.84, 0.92).into()),
+        transform: Transform::from_xyz(0.0, 0.5, 0.0),
+        ..default()
+    });
 }
