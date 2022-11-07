a7c34ad3d9e30475b67b6221bfbf35f42d4711d0
Alex <lepsalex@gmail.com>
2022-11-07 10:46:43 -0500

    get rid of magic strings

--- src/world/level.rs
+++ src/world/level.rs
@@ -26,6 +26,11 @@ pub struct Waypoint {
     pub spawn_id: u32,
 }
 
+// Names matching node name in Blender
+const TOWER_BASE_LOCATION_NAME: &str = "grass"; 
+const SPAWN_LOCATION_NAME: &str = "spawn"; 
+const WAYPOINT_LOCATION_NAME: &str = "waypoint"; 
+
 fn spawn_level(mut commands: Commands, game_assets: Res<GameAssets>) {
     // Spawn Level
     commands
@@ -40,18 +45,18 @@ fn spawn_level(mut commands: Commands, game_assets: Res<GameAssets>) {
                     /*
                     Attach required components for marked tiles
                     */
-                    if name.starts_with("grass") {
+                    if name.starts_with(TOWER_BASE_LOCATION_NAME) {
                         cmds.insert(TowerBaseLocation);
                     }
 
-                    if name.starts_with("spawn") {
+                    if name.starts_with(SPAWN_LOCATION_NAME) {
                         let data: Vec<&str> = name.split(".").collect();
                         cmds.insert(SpawnPoint {
                             id: data.get(1).unwrap().parse::<u32>().unwrap(),
                         });
                     }
 
-                    if name.starts_with("waypoint") {
+                    if name.starts_with(WAYPOINT_LOCATION_NAME) {
                         let data: Vec<&str> = name.split(".").collect();
                         cmds.insert(Waypoint {
                             id: data.get(1).unwrap().parse::<u32>().unwrap(),
